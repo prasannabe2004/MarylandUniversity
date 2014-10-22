79c326e9871a3e0f95b3b9adad8c0e8d4a414ebb
prasannabe2004 <prasannabe2004@gmail.com>
2014-10-22 12:35:15 -0500

    loss of precision example added.

---
+++ coursera/SoftwareSecurity/Week1/lossOfPrecision.c
@@ -0,0 +1,16 @@
+int main()
+{
+      int l;
+      short s;
+      char c;
+
+      l = 0xdeadbeef;
+      s = l;
+      c = l;
+
+      printf("l = 0x%x (%d bits)\n", l, sizeof(l) * 8);
+      printf("s = 0x%x (%d bits)\n", s, sizeof(s) * 8);
+      printf("c = 0x%x (%d bits)\n", c, sizeof(c) * 8);
+
+      return 0;
+}
